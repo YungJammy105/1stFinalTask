a09e671af933a82b05d7b97528e5b1635880e96a
Ilya Andreev <Franklylakers@yandex.ru>
2023-01-01 16:44:39 +0700

    added method PrintArray to code

--- FinalExample/Program.cs
+++ FinalExample/Program.cs
@@ -1,4 +1,9 @@
-﻿string[] InputStr()
+﻿void PrintArray(string[] arr)
+{
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+string[] InputStr()
 {
     Console.Write("Enter array values separated by spaces: ");
     string test = Console.ReadLine();
