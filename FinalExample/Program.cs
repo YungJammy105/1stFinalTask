682641955e71c3eead2a043d5acf0703bf7043af
Ilya Andreev <Franklylakers@yandex.ru>
2023-01-01 16:49:06 +0700

    added main code to the programm

--- FinalExample/Program.cs
+++ FinalExample/Program.cs
@@ -1,4 +1,32 @@
-﻿int NumberOfCharactersNotExceedingLength(string[] array)
+﻿Console.Clear();
+string[] arr = InputStr();
+Console.Write("Enter the maximum number of characters per string: ");
+int characterLength = int.Parse(Console.ReadLine());
+string[] FinalArray = OutputtingAnArrayWithValuesLessThanTwoCharacters(arr);
+Thread.Sleep(1000);
+Console.Clear();
+Console.WriteLine("Your initial array:");
+PrintArray(arr);
+Console.Write("Loading");
+Thread.Sleep(600);
+Console.Write(".");
+Thread.Sleep(600);
+Console.Write(".");
+Thread.Sleep(600);
+Console.Write(".");
+Thread.Sleep(1700);
+Console.WriteLine();
+if(FinalArray.Length > 0)
+{
+    Console.WriteLine($"Found values of length <= {characterLength}: {NumberOfCharactersNotExceedingLength(arr)}");
+    PrintArray(FinalArray);
+}
+else
+{
+    Console.WriteLine("Values not find");
+}
+
+int NumberOfCharactersNotExceedingLength(string[] array)
 {
     int character = 0;
     for (int i = 0; i < array.Length; i++)
